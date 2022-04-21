e504f786fd14849fd593c589e5d8c47aa62c7622
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-04-20 21:36:57 -0300

    Video 6 Terminado

--- Assets/Scripts/ValidarContrasenia.cs
+++ Assets/Scripts/ValidarContrasenia.cs
@@ -8,11 +8,14 @@ public class ValidarContrasenia : MonoBehaviour
     string contraseniaCorrecta;
     string contraseniaUsuario;
     public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
 
     // Start is called before the first frame update
     void Start()
     {
         contraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,11 +30,15 @@ public class ValidarContrasenia : MonoBehaviour
 
         if(contraseniaUsuario == contraseniaCorrecta)
         {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
             Debug.Log("Bienvenido");
         }
 
         else
         {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña Incorrecta";
             Debug.Log("Contraseña Incorrecta");
         }
     }
